a782152668f7b6ca3308b5e8bec70dbf0f10c8e3
zellytozelly <zellytozelly@gmail.com>
2022-05-28 05:30:34 +0900

    Refactor: Refactor userManagement
    Co-authored-by: LUMPEN <lumpenop@users.noreply.github.com>
    Co-authored-by: hsw824 <hsw824@users.noreply.github.com>

--- src/routes/BackOffice/User/UserManagement/index.tsx
+++ src/routes/BackOffice/User/UserManagement/index.tsx
@@ -1,14 +1,8 @@
 import { useState } from 'react'
-import { useRecoil } from 'hooks/state'
-import { userListState } from 'store/userManagement'
 import UserList from './comp/UserList'
 import UserSearch from './comp/UserSearch'
-import { IUser } from 'types/userManagement'
 
-interface Props {}
-
-const UserManagement = (props: Props) => {
-  const [userList, setUserList] = useRecoil<IUser[]>(userListState)
+const UserManagement = () => {
   const [isListHidden, setIsListHidden] = useState(false)
 
   return (
