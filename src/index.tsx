b9886115a9cfd5654a196f7c96e5c88a30aefe5b
bugeon <bearbu@naver.com>
2022-05-28 23:32:34 +0900

    Style: Delete comment
    Co-authored-by: Lee Ju I <jui9266@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -9,63 +9,17 @@ import reportWebVitals from './reportWebVitals'
 import './styles/index.scss'
 
 import Routes from './routes'
-
-// const idbConfig = {
-//   databaseName: 'moadata-db',
-//   version: 1,
-//   stores: [
-//     {
-//       name: 'Admin',
-//       id: { keyPath: 'id', autoIncrement: true },
-//       indices: [
-//         { name: 'admin_id', keyPath: 'admin_id' },
-//         { name: 'password', keyPath: 'password' },
-//       ],
-//     },
-//     {
-//       name: 'User',
-//       id: { keyPath: 'id', autoIncrement: true },
-//       indices: [
-//         { name: 'user_id', keyPath: 'user_id' },
-//         { name: 'seq', keyPath: 'seq', options: { unique: true } },
-//         { name: 'created_date', keyPath: 'created_date' },
-//       ],
-//     },
-//     {
-//       name: 'HeartRate',
-//       id: { keyPath: 'id', autoIncrement: true },
-//       indices: [
-//         { name: 'seq', keyPath: 'seq' },
-//         { name: 'member_seq', keyPath: 'member_seq' },
-//         { name: 'avg_beat', keyPath: 'avg_beat' },
-//         { name: 'crt_ymdt', keyPath: 'crt_ymdt' },
-//       ],
-//     },
-//     {
-//       name: 'Step',
-//       id: { keyPath: 'id', autoIncrement: true },
-//       indices: [
-//         { name: 'seq', keyPath: 'seq' },
-//         { name: 'member_seq', keyPath: 'member_seq' },
-//         { name: 'steps', keyPath: 'steps' },
-//         { name: 'minutes', keyPath: 'minutes' },
-//         { name: 'distance', keyPath: 'distance' },
-//         { name: 'calorie', keyPath: 'calorie' },
-//         { name: 'crt_ymdt', keyPath: 'crt_ymdt' },
-//       ],
-//     },
-//   ],
-// }
+import { RecoilRoot } from 'recoil'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <IndexedDBProvider config={idbConfig}> */}
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-      {/* </IndexedDBProvider> */}
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </RecoilRoot>
     </Provider>
   </React.StrictMode>
 )
