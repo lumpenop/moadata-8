dec2ec44e9d705be0152ece8df0d08037ebb4f96
zellytozelly <zellytozelly@gmail.com>
2022-06-02 01:50:17 +0900

    Fix: Fix PR error
    Co-authored-by: LUMPEN <lumpenop@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { useRecoilValue } from 'recoil'
 import User from './BackOffice/User'
